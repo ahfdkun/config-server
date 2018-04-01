eeb1c9468396cb497b38d5c2867607ebdde4874b
ahfdkun <785440131@qq.com>
2018-04-01 20:53:53 +0800

    create config server

---
+++ src/main/java/com/ahfdkun/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.ahfdkun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer // 开启Spring Cloud Config服务端功能
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
