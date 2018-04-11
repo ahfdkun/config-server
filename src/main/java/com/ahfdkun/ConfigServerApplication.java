5eb1995bae1c10279a8a16433a37861639e9acf8
ahfdkun <785440131@qq.com>
2018-04-11 20:58:43 +0800

    服务化注册中心

--- src/main/java/com/ahfdkun/ConfigServerApplication.java
+++ src/main/java/com/ahfdkun/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.ahfdkun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer // 开启Spring Cloud Config服务端功能
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
